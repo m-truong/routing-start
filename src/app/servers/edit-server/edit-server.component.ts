e07c27a3bff58931410dec291bfae588414634e0
Michael Truong <truong.michael.2013@gmail.com>
2023-01-07 15:18:07 -0500

    Add route params and fragment support in edit-server file

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { ServersService } from '../servers.service';
 
@@ -7,14 +9,20 @@ import { ServersService } from '../servers.service';
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, OnDestroy {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  queryParamsSubscription: any;
+  fragmentSubscription: any;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private currRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log('logging', this.currRoute.snapshot.queryParams);
+    console.log('logging', this.currRoute.snapshot.fragment);
+    this.queryParamsSubscription = this.currRoute.queryParams.subscribe();
+    this.fragmentSubscription = this.currRoute.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
@@ -22,6 +30,11 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+  ngOnDestroy() {
+    this.queryParamsSubscription.unsubscribe();
+    this.fragmentSubscription.unsubscribe();
   }
 
 }
