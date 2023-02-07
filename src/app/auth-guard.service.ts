27109c6d8790a3c277b28f75a1381bb76801fa93
Michael Truong <truong.michael.2013@gmail.com>
2023-02-07 11:45:48 -0500

    Add AuthService Observable feature

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,9 +1,14 @@
+import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from "./auth.service";
 
 // CanActivate can run synchronously or asynchronously
 
+@Injectable()
 export class AuthGuard implements CanActivate {
+
+constructor(private authService: AuthService) {}
   // Note: This is run before the route gets activated
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
