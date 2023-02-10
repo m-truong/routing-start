19944fc72a22eb560e0a6d0db8bf45dcd9a8f273
Michael Truong <truong.michael.2013@gmail.com>
2023-02-10 16:52:52 -0500

    Add error message component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
@@ -28,8 +29,11 @@ const appRoutes: Routes = [
       { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ] 
   },
+  // {
+  //   path: 'not-found', component: PageNotFoundComponent
+  // },
   {
-    path: 'not-found', component: PageNotFoundComponent
+    path: 'not-found', component: ErrorPageComponent, data: { message: 'Page not found!' }
   },
   // {
   //   path: 'something', redirectTo: '/not-found'
