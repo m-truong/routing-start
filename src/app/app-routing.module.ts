fab8bd020e21b85d2adbf3fc6af2ca9d9bfc5ee8
Michael Truong <truong.michael.2013@gmail.com>
2023-02-14 15:47:44 -0500

    Add ServerResolver component to use instead

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent,
     children: [
       // Note: this is now a dynamic path that takes a 'url' 'parameter'
-      { path: ':arbVarURLparameter/:name', component: UserComponent },
+      { path: ':arbVarURLparameter/:name', component: UserComponent, resolve: {server: ServerResolver} },
     ]
   },
   // Now 'servers' is only activated/accessible if the login method is invoked
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -18,14 +18,21 @@ export class ServerComponent implements OnInit {
     ) { }
 
   ngOnInit() {
-    const id = +this.currRoute.snapshot.params['id'];
-    this.server = this.serversService.getServer(1);
-    this.currRoute.params
-    .subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id'])
-      }
-    )
+    this.currRoute.data
+      .subscribe(
+        (data: Data) => {
+          this.server = data['server'];
+        }
+      );
+
+    // const id = +this.currRoute.snapshot.params['id'];
+    // this.server = this.serversService.getServer(1);
+    // this.currRoute.params
+    // .subscribe(
+    //   (params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id'])
+    //   }
+    // );
   }
 
   onEdit() {
